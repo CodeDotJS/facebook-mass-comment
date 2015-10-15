7bf2399b22333daa3b7532adb14e07071d9f5e50
Rishi Giri <rushforlinux@gmail.com>
2015-10-16 03:27:53 +0530

    Update comments.py

--- comments.py
+++ comments.py
@@ -14,15 +14,15 @@ def comment(url):
     for x in xrange(1000):         
             
             print 'commenting %d '% x
-            path ='/'+'PUT FB STATUS ID HERE'+'/comments'
+            path ='/'+'PUT FB STATUS ID HERE'+'/comments' # you have to put the digits after ?fbid before '/comments'
             param_data={  'format':'json', 
-                        'message':'Auto Generated Comment', 
+                        'message':'Auto Generated Comment', # you can change this message. 
                         'access_token':access_token 
                   } 
             connect = httplib.HTTPSConnection("graph.facebook.com") 
             connect.request("POST",path,urllib.urlencode(param_data),{}) 
             time.sleep(0.09)
            
-url='/PUT FB STATUS ID HERE'
+url='/PUT FB STATUS ID HERE' # same as you have done at line 17
 comment(url) 
 print 'DONE!'
