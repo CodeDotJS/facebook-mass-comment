208d85dd77a9df1948148e84757f3c79e3cc9203
Rishi Giri <rushforlinux@gmail.com>
2016-01-29 18:25:55 +0530

    Update comments.py

--- comments.py
+++ comments.py
@@ -4,19 +4,21 @@ import os
 import json
 import time
 
-access_token='ACCESS TOKEN PLEASE!'
+access_token='Paste your access token here'
+# access_token = raw_input('Paste your access token here :')
+
 conn = httplib.HTTPSConnection("graph.facebook.com") 
 print 'Please Wait!'
  
 def comment(url): 
     connect = httplib.HTTPSConnection("graph.facebook.com") 
     connect.request("GET",url) 
-    for x in xrange(1000):  
+    for x in xrange(10): # make it 10, 100, 1000, 10000  
             
             print 'commenting %d '% x
             path ='/'+'PUT FB STATUS ID HERE'+'/comments'
             param_data={  'format':'json', 
-                        'message':'Auto Generated Comment',
+                        'message':'<3', # change message from here
                         'access_token':access_token 
                   } 
             connect = httplib.HTTPSConnection("graph.facebook.com") 
